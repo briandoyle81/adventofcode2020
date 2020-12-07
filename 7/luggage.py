8d386d06a4cdc6b789b77de8425d4853af4c7b70
Brian  Doyle <briandoyle81@gmail.com>
2020-12-07 08:24:21 -0600

    parsed rules for 7

---
+++ 7/luggage.py
@@ -0,0 +1,63 @@
+# should be using with...
+with open('data.txt', 'r') as file:
+    luggage_rules_data = file.read().split('\n')
+
+# cut trailing newline
+luggage_rules_data.pop(-1)
+
+
+# need to parse the rules into a logical data structure
+
+# all bags have adjective, then color
+
+# Some cleanup needed
+
+# rule format is bag, word "contains", either comma separated
+# or "contain no other bags"
+
+# dictionary with list of bags contained:
+
+# or dict of dicts, because we have quantities
+
+# sample = {
+#     "light red": {
+#         "bright white": 1,
+#         "muted yellow": 2
+#     }
+# }
+
+# do this then use to figure out permutations.  or is it combinations?
+
+rules_dict = dict()
+
+for rule in luggage_rules_data:
+    new_rule = dict()
+    rule = rule.strip('.')
+    split = rule.split(' contain ')
+    outer_bag = split[0][:-5]
+
+    contained_bags_string = split[1]
+
+    if contained_bags_string != 'no other bags':
+        contained_bags_list = contained_bags_string.split(', ')
+        for contained in contained_bags_list:
+            # only single digit numbers of bags so easy to parse
+            # first digit is amount
+            amount = contained[0]
+            name = contained[2:-5]
+            new_rule[name] = int(amount)
+
+    rules_dict[outer_bag] = new_rule
+
+print(rules_dict)
+
+# Now to find how many colors can eventually carry a gold bag.
+
+# Rules list for colors is also the list of colors, so iterate through that
+
+# counter
+# For each rule in rules_dict
+# Continue if shiny gold, needs to be in a bag
+# if rule contains shiny gold bag, + 1 and continue
+
+# This should/could be recursive.  Or can use while rule not empty
