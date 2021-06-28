a0b075431e5bfa1a3d754872b4b488d15ec95cb7
Brian  Doyle <briandoyle81@gmail.com>
2021-06-28 19:06:12 -0400

    solve part 2

--- 18/math.py
+++ 18/math.py
@@ -59,9 +59,36 @@ def eval_expression(exp):
     # we should always end with a number, so append the last one
     numbers.append(int(new_num_str))
 
+
+
+    # [1, 2, 3, 4, 5, 6]
+    # [+, *, +, *, +]
+
+    # do addition first
+
+    i = 0 # python
+    while i < len(operations):
+        # breakpoint()
+        if operations[i] == '*':
+            i += 1
+            continue
+        else:
+            # left operand shares same i
+            # add the two numbers
+            new_num = math(numbers[i], numbers[i+1], operations[i])
+            # replace first operand with new number
+            numbers[i] = new_num
+            # delete the second
+            del numbers[i+1]
+            # remover the operator
+            del operations[i]
+            # short problems so just reset the loop
+            i = 0
+
     result = numbers.pop(0)
 
-    while len(operations) and len(numbers) > 0:
+    # process multiplication
+    while len(numbers) > 0:
         op = operations.pop(0)
 
         b = numbers.pop(0)
@@ -107,7 +134,7 @@ def solve_problem(problem):
 
     return eval_expression(stack)
 
-# print(solve_problem('1 + (2 * 3) + (4 * (5 + 6))'))
+# print(solve_problem('1 + 2 * 3 + 4 * 5 + 6'))
 
 total = 0
 
@@ -115,3 +142,47 @@ for problem in data:
     total += int(solve_problem(problem))
 
 print(total)
+
+
+# Part 1 Evaluate Expression:
+
+# def eval_expression(exp):
+#     debug_original_exp = exp
+
+#     result = 0
+
+#     numbers = []
+#     operations = []
+
+#     new_num_str = ""
+
+#     while len(exp) > 0:
+#         char = exp[0]
+#         exp = exp[1:]
+
+#         try:
+#             int(char) # no idea of speed of this vs ord().  Prob slower
+#             new_num_str += char
+
+#         except ValueError:
+#             if len(new_num_str) > 0:
+#                 # no division, only ints for now
+#                 numbers.append(int(new_num_str))
+#                 new_num_str = ""
+#             operations.append(char)
+
+
+#     # we should always end with a number, so append the last one
+#     numbers.append(int(new_num_str))
+
+#     result = numbers.pop(0)
+
+#     while len(operations) and len(numbers) > 0:
+#         op = operations.pop(0)
+
+#         b = numbers.pop(0)
+
+#         result = math(result, b, op)
+
+#     # print(debug_original_exp, ' == ', result)
+#     return str(result)
